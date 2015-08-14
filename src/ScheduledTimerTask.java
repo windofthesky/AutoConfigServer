00a62ad9c09625a6e88451c3f85f1e02003ca16a
Hades.Yang <windofthesky@aliyun.com>
2015-08-14 09:16:26 +0800

    Update ScheduledTimerTask.java
    Modify the code style.

--- src/ScheduledTimerTask.java
+++ src/ScheduledTimerTask.java
@@ -24,17 +24,17 @@ import redis.clients.jedis.exceptions.JedisConnectionException;
  */
 public class ScheduledTimerTask extends TimerTask
 {
-	/**
+    /**
      * @FieldName: configdb.
      * @Description: the ConfigServer database client.
      */
-	private ConfigDBClient configdb;
+    private ConfigDBClient configdb;
 	
-	/**
+    /**
      * @FieldName: channel.
      * @Description: the channel which is used to publish message of application.
      */
-	private String channel;
+    private String channel;
 	
 	
     /**
@@ -43,75 +43,75 @@ public class ScheduledTimerTask extends TimerTask
      * @param chnl: the channel on the redis server. 
      * @return none.
      */
-	public void setChannel(String chnl)
-	{
-		this.channel = chnl;
-	}
+    public void setChannel(String chnl)
+    {
+	this.channel = chnl;
+    }
 
-	@Override
-	public void run()
-	{
-		//System.out.println("Time's up!!");
-		
+    @Override
+    public void run()
+    {
+	//System.out.println("Time's up!!");
+	
         Jedis jedisConnector = null;
-		boolean borrowOrOprSuccess = true;
+	boolean borrowOrOprSuccess = true;
+	
+	try 
+	{
+	    jedisConnector = configdb.db_client.getResource();
+	    jedisConnector.publish(channel, "this is a test message");
+	    /*
+	     * if user application has some monitor parameters,they should be got here 
+	     * and then publish them to specific monitor channels. 
+	     * the code shoule be as below:
+	     *
+	     * if(channel.equals("OnLine_UserNum"))
+	     * {
+	     *     String current_online_user_num = app.getOnlineUserNum();
+	     *     jedisConnector.publish(channel, current_online_user_num); //publish the online user num.
+	     * } 
+	     *
+	     * if user application store the history data of the parameter,the redis command should 
+	     * like these below:
+	     * if(channel.equals("OnLine_UserNum"))
+	     * {
+	     *     String current_online_user_num = app.getOnlineUserNum();
+	     *     String current_time = app.getCurrentTime();
+	     *     jedisConnector.hset("ONLINE_USERNUM", current_time, current_online_user_num);
+	     * } 
+	     *
+	     */
 		
-		try 
-		{
-			jedisConnector = configdb.db_client.getResource();
-			jedisConnector.publish(channel, "this is a test message");
-			/*
-		     * if user application has some monitor parameters,they should be got here 
-		     * and then publish them to specific monitor channels. 
-		     * the code shoule be as below:
-		     *
-		     * if(channel.equals("OnLine_UserNum"))
-		     * {
-		     *     String current_online_user_num = app.getOnlineUserNum();
-		     *     jedisConnector.publish(channel, current_online_user_num); //publish the online user num.
-		     * } 
-		     *
-		     * if user application store the history data of the parameter,the redis command should 
-		     * like these below:
-		     * if(channel.equals("OnLine_UserNum"))
-		     * {
-		     *     String current_online_user_num = app.getOnlineUserNum();
-		     *     String current_time = app.getCurrentTime();
-		     *     jedisConnector.hset("ONLINE_USERNUM", current_time, current_online_user_num);
-		     * } 
-		     *
-		     */
-			
-		}
-		catch(JedisConnectionException e)
-		{
-			borrowOrOprSuccess = false;
-			if(jedisConnector != null)
-			{
-				configdb.db_client.returnBrokenResource(jedisConnector);
-				jedisConnector = null;
-			}
-			throw e;
-		}
-		finally
-		{
-			if(borrowOrOprSuccess && (jedisConnector!=null))
-			{
-				configdb.db_client.returnResource(jedisConnector);
-			}
-		}
 	}
+	catch(JedisConnectionException e)
+	{
+	    borrowOrOprSuccess = false;
+            if(jedisConnector != null)
+	    {
+		configdb.db_client.returnBrokenResource(jedisConnector);
+		jedisConnector = null;
+    	    }
+	    throw e;
+	}
+	finally
+	{
+	    if(borrowOrOprSuccess && (jedisConnector!=null))
+	    {
+		configdb.db_client.returnResource(jedisConnector);
+	    }
+	}
+    }
 	
     /**
      * @Title: ScheduledTimerTask.
      * @Description: the construct function which is used to initialize the object.
-	 * @param serverinfo_0: the first redis server node ip & port information in the redis sentinel.
-	 * @param serverinfo_1: the second redis server node ip & port information in the redis sentinel.
+     * @param serverinfo_0: the first redis server node ip & port information in the redis sentinel.
+     * @param serverinfo_1: the second redis server node ip & port information in the redis sentinel.
      * @param serverinfo_2: the third redis server node ip & port information in the redis sentinel.
      * @return none.
      */
-	public ScheduledTimerTask(String serverinfo_0, String serverinfo_1, String serverinfo_2)
-	{
-		this.db = new ConfigDBClient(serverinfo_0, serverinfo_1, serverinfo_2);
-	}
+    public ScheduledTimerTask(String serverinfo_0, String serverinfo_1, String serverinfo_2)
+    {
+	this.db = new ConfigDBClient(serverinfo_0, serverinfo_1, serverinfo_2);
+    }
 }
