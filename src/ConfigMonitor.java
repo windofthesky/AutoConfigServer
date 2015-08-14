2b01d7efbe7b663062f6000d4b746bb2b3bb52af
Hades.Yang <windofthesky@aliyun.com>
2015-08-14 09:00:07 +0800

    Update ConfigMonitor.java
    Modify the code style.

--- src/ConfigMonitor.java
+++ src/ConfigMonitor.java
@@ -20,44 +20,44 @@ package autoconfigserver;
  */
 public class ConfigMonitor
 {
-	/**
+    /**
      * @FieldName: sub.
      * @Description: the subscriber instance for monitor fetch user application info.
      */
-	private Subscriber sub;
+    private Subscriber sub;
 	
     /**
      * @FieldName: pub.
      * @Description: the publisher instance for monitor send the config info to the ConfigClient.
      */
-	private Publisher pub;
+    private Publisher pub;
 	
-	/**
+    /**
      * @Title: main.
      * @Description: main function of the config monitor.
      * @param args: input arguments of the main function. 
      * @return none.
      */
-	public static void main(String[] args)
-	{
-		/**
-		 * Subscriber is used to get monitor parameter from ConfigClient,which is running on the user application.
-		 * For example:
-		 * if ConfigMonitor want to monitor some parameter and the code should be written like below:
-		 *
-		 * CPU_LOAD = new Subscriber("10.1.1.178", "CPU_Load"); 
-		 *
-		 * if ConfigMonitor wand to config the ConfigClient,here should be a Publisher,code as below: 
-		 * 
-		 * Regist_Speed = new Publisher("10.1.1.178", "REGIST_SPEED");  //the ConfigClient will subscribe the channel.
-		 *
-		 * and if Administrator have a new config value to be sent to ConfigClient,code as below:
-		 *
-		 * Regist_Speed.sendConfig("500");  //Set regist speed to be 500/s. 
-		 */
-		sub = new Subscriber("10.1.1.41:6379", "10.1.1.41:6379", "10.1.1.41:6379", "TEST");
-		pub = new Publisher("10.1.1.41:6379", "10.1.1.41:6379", "10.1.1.41:6379", "TEST");
-		
-		pub.sendConfig("this is a test!");
-	}
+    public static void main(String[] args)
+    {
+	/**
+	 * Subscriber is used to get monitor parameter from ConfigClient,which is running on the user application.
+	 * For example:
+	 * if ConfigMonitor want to monitor some parameter and the code should be written like below:
+	 *
+	 * CPU_LOAD = new Subscriber("10.1.1.178", "CPU_Load"); 
+	 *
+	 * if ConfigMonitor wand to config the ConfigClient,here should be a Publisher,code as below: 
+	 * 
+	 * Regist_Speed = new Publisher("10.1.1.178", "REGIST_SPEED");  //the ConfigClient will subscribe the channel.
+	 *
+	 * and if Administrator have a new config value to be sent to ConfigClient,code as below:
+	 *
+	 * Regist_Speed.sendConfig("500");  //Set regist speed to be 500/s. 
+	 */
+	sub = new Subscriber("10.1.1.41:6379", "10.1.1.41:6379", "10.1.1.41:6379", "TEST");
+	pub = new Publisher("10.1.1.41:6379", "10.1.1.41:6379", "10.1.1.41:6379", "TEST");
+	
+	pub.sendConfig("this is a test!");
+    }
 }
